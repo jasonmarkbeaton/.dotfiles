c29bb32824026ae277b3d8e9c6c6bf50fd2be911
jason.beaton <jason.beaton@gmail.com>
2019-10-18 14:42:30 -0400

    Added Time Machine rules
    No backups on local hard drive

--- osx/defaults.sh
+++ osx/defaults.sh
@@ -107,6 +107,16 @@ defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
 ###############################################################################
+# Time Machine                                                                #
+###############################################################################
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Disable local Time Machine backups
+hash tmutil &> /dev/null && sudo tmutil disablelocal
+
+###############################################################################
 # Transmission.app                                                            #
 ###############################################################################
 
